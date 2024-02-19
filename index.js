19761b9d4899408d14177cdb132fcff9dcf37a8e
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2024-02-19 22:01:25 +0530

    feat: Add code to find and display habitable planets

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import { parse } from "csv-parse";
 import * as fs from 'node:fs';
 
-const records = [];
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+    return (
+        (planet.koi_disposition === 'CONFIRMED') &&
+        (planet.koi_insol > 0.36 && planet.koi_insol < 1.11) &&
+        (planet.koi_prad < 1.6)
+    );
+}
 
 fs.createReadStream('./data/kepler_data.csv')
     .pipe(parse({
@@ -9,13 +17,16 @@ fs.createReadStream('./data/kepler_data.csv')
         comment: '#',
     }))
     .on('data', (data) => {
-        records.push(data);
+        if (isHabitablePlanet(data)) {
+            habitablePlanets.push(data);
+        }
     })
     .on('error', (err) => {
         console.log('Error while reading file', err);
     })
     .on('end', () => {
-        console.log('Done with reading file!');
-        console.log(`Found ${records.length} records`);
-        // console.log('records', records);
+        console.log('Read and processed planets data file.');
+        console.log(`Found ${habitablePlanets.length} habitable planets.`);
+        console.log('They are...');
+        habitablePlanets.map(planet => console.log(planet.kepler_name));
     });
