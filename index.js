26ab7c1b3d306da80804743d59786b59bc6c80f4
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2024-02-19 19:49:16 +0530

    feat: Read data using built-in File System module of Node.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 import { parse } from "csv-parse";
+import * as fs from 'node:fs';
+
+const records = [];
+
+// Here we are using built-in File System module for the Node.js to read the data file.
+// It reads raw data of the file, it does not parse it.
+fs.createReadStream('./data/kepler_data.csv')
+    .on('data', (data) => {
+        records.push(data);
+    })
+    .on('error', (err) => {
+        console.log('Error while reading file', err);
+    })
+    .on('end', () => {
+        console.log('Done with reading file!');
+        console.log(`Found ${records.length} records`);
+        console.log('records', records);
+    });
